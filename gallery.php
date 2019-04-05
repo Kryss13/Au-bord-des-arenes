508f0303f8dd7b0ffaf5c9b26c6afd7226feee46
Chris <ch.hecquet@live.fr>
2019-04-05 11:10:52 +0200

    add gallery

---
+++ gallery.php
@@ -0,0 +1,26 @@
+<?php
+$title = 'Galerie photos';
+include ('include/header.phtml');
+ ?>
+<!-- CSS Gallery -->
+<link rel="stylesheet" href="css/lightbox.min.css"/>
+<script type="text/javascript" src="js/lightbox-plus-jquery.min.js"></script>
+<h1>Galerie photos</h1>
+
+<div class="gallery">
+  <a href="images/gallery/chambre_1.jpg" data-lightbox="mygallery"><img src="images/gallery/chambre_1_small.jpg" alt="chambre"></a>
+  <a href="images/gallery/chambre_2.jpg" data-lightbox="mygallery"><img src="images/gallery/chambre_2_small.jpg" alt="chambre"></a>
+  <a href="images/gallery/chambre_3.jpg" data-lightbox="mygallery"><img src="images/gallery/chambre_3_small.jpg" alt="chambre"></a>
+  <a href="images/gallery/chambre_4.jpg" data-lightbox="mygallery"><img src="images/gallery/chambre_4_small.jpg" alt="chambre"></a>
+  <a href="images/gallery/cuisine_1.jpg" data-lightbox="mygallery"><img src="images/gallery/cuisine_1_small.jpg" alt="cuisine"></a>
+  <a href="images/gallery/cuisine_2.jpg" data-lightbox="mygallery"><img src="images/gallery/cuisine_2_small.jpg" alt="cuisine"></a>
+  <a href="images/gallery/dressing.jpg" data-lightbox="mygallery"><img src="images/gallery/dressing_small.jpg" alt="dressing"></a>
+  <a href="images/gallery/mezzanine_1.jpg" data-lightbox="mygallery"><img src="images/gallery/mezzanine_1_small.jpg" alt="mezzanine"></a>
+  <a href="images/gallery/mezzanine_2.jpg" data-lightbox="mygallery"><img src="images/gallery/mezzanine_2_small.jpg" alt="mezzanine"></a>
+  <a href="images/gallery/sdb_1.jpg" data-lightbox="mygallery"><img src="images/gallery/sdb_1_small.jpg" alt="salle de bain"></a>
+  <a href="images/gallery/sdb_2.jpg" data-lightbox="mygallery"><img src="images/gallery/sdb_2_small.jpg" alt="salle de bain"></a>
+  <a href="images/gallery/sejour_1.jpg" data-lightbox="mygallery"><img src="images/gallery/sejour_1_small.jpg" alt="sejour"></a>
+  <a href="images/gallery/sejour_2.jpg" data-lightbox="mygallery"><img src="images/gallery/sejour_2_small.jpg" alt="sejour"></a>
+</div>
+
+<?php include('include/footer.phtml'); ?>
